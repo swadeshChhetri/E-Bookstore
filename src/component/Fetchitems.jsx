f265641acf354faf26a9d72613e5cc21da8aa6cc
Swadesh Chhetri <swadeshchhetri87@gmail.com>
2024-08-11 12:29:35 +0530

    update fetching process

--- src/component/Fetchitems.jsx
+++ src/component/Fetchitems.jsx
@@ -16,31 +16,28 @@ const Fetchbooks = () => {
   useEffect(() => {
     if (fetchStatus.fetchDone) return;
 
-    const controller = new AbortController();
-    const signal = controller.signal;
-
     const fetchData = async () => {
       dispatch(fetchStatusActions.markFetchingStarted());
 
       try {
         const [booksRes, historyRes, poetryRes, psychologyRes, artsRes, 
-storiesRes, motivationRes] = await Promise.all([
-          fetch("http://localhost:3006/books", { signal }),
-          fetch("http://localhost:3006/history", { signal }),
-          fetch("http://localhost:3006/poetry", {signal} ),
-          fetch("http://localhost:3006/psychology", {signal}),
-          fetch("http://localhost:3006/arts", {signal}),
-          fetch("http://localhost:3006/stories", {signal} ),
-          fetch("http://localhost:3006/motivation", {signal} )
+        storiesRes, motivationRes] = await Promise.all([
+          fetch("https://swadeshchhetri.github.io/bookstore-api/books"),
+          fetch("https://swadeshchhetri.github.io/bookstore-api/history"),
+          fetch("https://swadeshchhetri.github.io/bookstore-api/poetry"),
+          fetch("https://swadeshchhetri.github.io/bookstore-api/psychology"),
+          fetch("https://swadeshchhetri.github.io/bookstore-api/arts"),
+          fetch("https://swadeshchhetri.github.io/bookstore-api/stories"),
+          fetch("https://swadeshchhetri.github.io/bookstore-api/motivation")
         ]);
 
-        if (!booksRes.ok || !historyRes.ok || !poetryRes || !psychologyRes ||
-!artsRes ||!storiesRes ||!motivationRes) {
+        if (!booksRes.ok || !historyRes.ok || !poetryRes.ok || !psychologyRes.ok ||
+            !artsRes.ok || !storiesRes.ok || !motivationRes.ok) {
           throw new Error("Failed to fetch data");
         }
 
         const [books, history, poetry, psychology, arts, stories, motivation] = 
-await Promise.all([
+        await Promise.all([
           booksRes.json(),
           historyRes.json(),
           poetryRes.json(),
@@ -49,6 +46,7 @@ await Promise.all([
           storiesRes.json(),
           motivationRes.json()
         ]);
+
         dispatch(booksActions.addInitialbooks(books));
         dispatch(historyActions.addInitialhistory(history));
         dispatch(poetryActions.addInitialpoetry(poetry));
@@ -69,11 +67,11 @@ await Promise.all([
     fetchData();
 
     return () => {
-      //controller.abort();
+
     };
   }, [fetchStatus, dispatch]);
 
-  return null; // or any JSX you want to render
+  return null;
 };
 
 export default Fetchbooks;
